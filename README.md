ecb34265ecc3e3b6cb66c17e99940b3d5c56883e
Christian Neuhäuser <chris@type10.com>
2018-01-04 14:52:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+notify
+======
+
+> Send CD / CI notifications
+
+This 
+
+## Install
+
+```sh
+$ npm install -g @maxdome/notify
+```
+
+## Usage
+
+```
+  Usage: notify [options] [command]
+
+
+  Options:
+
+    -V, --version  output the version number
+    -v --verbose   Be more verbose
+    -h, --help     output usage information
+
+
+  Commands:
+
+    hipchat [options]   Send HipChat notification
+```
+
+## TODO
+
+More integrations coming soon:
+
+- Microsoft Teams
+- E-mail
---
+++ src/cli.js
@@ -0,0 +1,133 @@
+const program = require('commander');
+const pkg = require('../package.json');
+const got = require('got');
+const path = require('path');
+const fs = require('fs');
+
+module.exports = argv => {
+  program.version(pkg.version);
+  program.option('-v --verbose', 'Be more verbose');
+
+  function msg() {
+    console.log(...arguments);
+  }
+
+  function handleError(err) {
+    err = err instanceof Error ? err : new Error(err);
+    console.error(program.verbose ? err : `Error: ${err.message}`);
+    process.exit(1);
+  }
+
+  function handleResult(text, code) {
+    code = code || 0;
+
+    if (text) {
+      msg(text);
+    }
+
+    process.exit(code);
+  }
+
+  function render(strings, ...values) {
+    let out = '';
+    strings.forEach((str, i) => {
+      out += `${str}${values[i] || ''}`;
+    });
+    return out;
+  }
+
+  function getTemplate(template, data) {
+    const file = template ? path.join(process.cwd(), template) : path.join(__dirname, '../tpl/deploy.html');
+    let message;
+    try {
+      const func = new Function('return `' + fs.readFileSync(file, 'utf8') + '`;');
+      message = render`${func.call(data)}`;
+    } catch (e) {
+      handleError(e);
+    }
+    return message;
+  }
+
+  process.on('uncaughtException', err => {
+    msg(err.message);
+    if (program.verbose) {
+      msg(err.stack);
+    }
+    process.exit(1);
+  });
+
+  function checkRequiredOptions(options) {
+    Object.values(options.options).forEach(option => {
+      const name = option.long.slice(2);
+      if (option.required && !options.hasOwnProperty(name)) {
+        handleError(`option '${option.flags}' missing`);
+      }
+    });
+  }
+
+  program
+    .command('hipchat')
+    .description('Send HipChat notification')
+    .option('--appName <appName>', 'Application name')
+    .option('--jiraFixVersion <jiraFixVersion>', 'JIRA fix version')
+    .option('--label <label>', 'Elastic Beanstalk label')
+    .option('--ciEnvName <ciEnvName>', 'CI environment name')
+    .option('--ciEnvUrl <ciEnvUrl>', 'CI environment URL')
+    .option('--ciProjectUrl <ciEnvUrl>', 'CI project URL')
+    .option('--ciPipelineId <ciPipelineId>', 'CI pipeline ID')
+    .option('--from [from]', 'Notification sender name')
+    .option('--color [color]', 'Notification color [yellow, green, red, purple, gray, random]')
+    .option('--format [format]', 'Notification format [text|html]')
+    .option('--silent', 'Disable notification alert')
+    .option(
+      '--hipChatToken [hipChatToken]',
+      'HipChat bearer token. Prefer HIPCHAT_TOKEN env variable over this option!'
+    )
+    .option('--template [template]', 'Path to notification template literal')
+    .action(async options => {
+      checkRequiredOptions(options);
+
+      const data = Object.assign(
+        {
+          jiraBaseUrl: 'https://jira.sim-technik.de/issues/?jql=$JIRA_QUERY%20AND%20fixVersion%3D',
+          appName: 'unknown',
+          jiraFixVersion: 'unknown',
+          label: 'unknown',
+          ciEnvUrl: 'unknown',
+          ciEnvName: 'unknown',
+          ciProjectUrl: 'unknown',
+          ciPipelineId: 'unknown',
+        },
+        options
+      );
+
+      const headers = {
+        Authorization: `Bearer ${options.hipChatToken || process.env.HIPCHAT_TOKEN}`,
+      };
+
+      const body = {
+        from: options.from || 'GitLab CI',
+        color: options.color || 'purple',
+        message_format: options.format || 'html',
+        notify: !options.silent,
+        message: getTemplate(options.template, data),
+      };
+
+      try {
+        await got.post(`https://api.hipchat.com/v2/room/${options.roomId || 'Public%20Deployments'}/notification`, {
+          json: true,
+          body,
+          headers,
+        });
+        handleResult('Notification sent.');
+      } catch (e) {
+        handleError(e);
+      }
+    });
+
+  program.parse(argv);
+
+  if (!argv.slice(2).length) {
+    program.help();
+  }
+};
